be8fa4adc7c7b289eb54d8c508eb088962d91d62
rui323 <ruiiwata837@gmail.com>
2025-04-27 22:52:27 +0900

    add drone gui system

---
+++ include/drone_operation/operator_gui_component.hpp
@@ -0,0 +1,112 @@
+#ifndef DRONE_GUI_COMPONENT_HPP
+#define DRONE_GUI_COMPONENT_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <chrono>
+#include <functional>
+#include <algorithm>
+
+
+#include <rclcpp/clock.hpp>
+#include <rclcpp/time.hpp>
+
+#include <opencv2/opencv.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <geometry_msgs/msg/point.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <rclcpp/type_adapter.hpp>
+
+#include "drone_operation/gui_tool.hpp"
+#include "drone_operation/cv_mat_type_adapter.hpp"
+
+using namespace std::chrono_literals;
+
+namespace component_operator_gui
+{
+class DroneGUI : public rclcpp::Node
+{
+public:
+    using MyAdaptedType = rclcpp::TypeAdapter<cv::Mat, sensor_msgs::msg::Image>;
+
+    // ボタンの設定-------------------------------------------------
+    cv::Mat mat;// ボタンの画像
+    // 描画設定
+    int width = 300, height = 350;//画面サイズ
+    int btn_width = 140, btn_height = 50;//ボタンのサイズ
+    int x_offset = 5,y_offset = 25;//ボタンの最初の位置
+    int btn_per_row = 2;//一行に何個ボタン設置するか
+    int btn_space_row = 5, btn_space_col = 30;//ボタン間のスペース
+
+    // 中身
+    std::vector<std::string> buttons_name;//表示しているボタンのリスト
+    std::vector<Button> buttons_; // ボタン位置、サイズのリスト
+    cv::Size btn_size = cv::Size(btn_width,btn_height);
+
+    // 最新の値を格納確認するリスト----------------------------------
+    // std::vector<std::map<std::string, bool>> receive_list;
+    std::string latest_topic = "None";
+    bool latest_qr = false;
+
+    // バルブの開閉状況カウント変数 open / close---------------------
+    int bulb_state_count = 0;
+
+    // 画像分配用---------------------------------------------------
+    std::map<std::string, bool> bool_flags_ = {
+        {"pressure",false},
+        {"qr",false},
+        {"cracks",false}
+    };// 連億処理信号とそれに対応する送り先のmap
+    std::map<std::string, rclcpp::Time> start_times_ = {
+        {"pressure",rclcpp::Time(0)},
+        {"qr",rclcpp::Time(0)},
+        {"cracks",rclcpp::Time(0)}
+    };// 信号を受け取ってから1secとカウントするため
+    double check_duration_sec;
+
+    // 受け取ったメッセージを格納-------------------------------------
+    std_msgs::msg::String id, result_data;// 確認ノードへ報告を行う時に必要なid 結果を格納
+    cv::Mat temporary_image;// sensor_msgsで送られてくるので一時的にcv::Matへ
+    std::unique_ptr<cv::Mat> result_image;// 確認ノードへ送信する画像
+
+    std::vector<std::string> trigger_list = {"pressure", "qr", "cracks"};
+    explicit DroneGUI(const rclcpp::NodeOptions &options);
+    DroneGUI() : DroneGUI(rclcpp::NodeOptions{}) {}
+
+private:
+    cv ::Mat setup();// ボタン画面生成生成
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg);// 検出結果をうけとった時に行う処理関数
+    void timer_callback();// 定期的にボタン画像を流す
+    void mouse_click_callback(const geometry_msgs::msg::Point::SharedPtr msg);// ボタン画面にクリックした時の座標をもとに行う処理関数
+    void rewriteButton(cv::Point sp, cv::Point ep, std::string text, int btn_W, int btn_H, cv::Scalar color) const;// 指定したボタンの色、表示内容を変更
+    void rewriteMessage();
+    void process(std::string topic_name);// クリックしたボタンに対応した処理を行う関数
+    void publish_images();// 連続処理ノードへ画像を流す関数
+
+    rclcpp::Publisher<MyAdaptedType>::SharedPtr publish_gui_;// ボタン画面を流すpublisher
+    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr click_;// ボタンクリック座標を受け取るsubscriber
+    rclcpp::TimerBase::SharedPtr view_;// ボタン画面を定期的に流すタイマー
+    rclcpp::TimerBase::SharedPtr timer_;// 一定間隔で画像を流す
+    rclcpp::TimerBase::SharedPtr color_reset_timer_;
+    
+    std::map<std::string, std::shared_ptr<rclcpp::Publisher<MyAdaptedType>>> image_publishers_;// 連続処理ノードに画像を流す
+
+    std::map<std::string, rclcpp::Subscription<std_msgs::msg::String>::SharedPtr> receive_data_;// 検出結果をうけとる　
+    std::map<std::string, rclcpp::Subscription<MyAdaptedType>::SharedPtr> receive_image_;// 検出画像をうけとる
+
+    rclcpp::Subscription<MyAdaptedType>::SharedPtr receive_raw_image_;// DRONEから来る生画像
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_qr_publisher_;// 確認ノードへidを送る
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_data_publisher_;// 確認ノードへ検出結果を送る
+    rclcpp::Publisher<MyAdaptedType>::SharedPtr dt_image_publisher_;// 確認ノードへ検出画像を送る
+    
+};
+
+} // namespace component_operator_gui
+
+#endif // DRONE_GUI_COMPONENT_HPP
---
+++ src/operator_gui_component.cpp
@@ -0,0 +1,289 @@
+#include "drone_operation/operator_gui_component.hpp"
+
+namespace component_operator_gui
+{
+DroneGUI::DroneGUI(const rclcpp::NodeOptions &options) 
+    : Node("drone_gui", options)
+{
+    // ミッションごとにモードを変更------------------------------------------------------------------------------------------
+    this->declare_parameter("mode", "P6");
+    std::string param = this->get_parameter("mode").as_string();
+    RCLCPP_INFO(this->get_logger(), "Received my_parameter: %s", param.c_str());
+    
+    // 画像送信時間、間隔をパラメータにする------------------------------------------------------------------------------------
+    this->declare_parameter<double>("check_duration_sec",1.0);
+    this->declare_parameter<int>("timer_interval_ms",100);
+
+    int timer_interval_ms;
+    this->get_parameter("check_duration_sec", check_duration_sec);
+    this->get_parameter("timer_interval_ms", timer_interval_ms);
+    RCLCPP_INFO_STREAM(this->get_logger(),"Interval: " << timer_interval_ms << " ms, duration_time: " << check_duration_sec);
+
+    // ミッションごとのボタン表示名------------------------------------------------------------------------------------------
+    std::map<std::string, std::vector<std::string>> pub_sub_topics = {
+        {"P1", {"pressure", "qr", "send"}},
+        {"P2", {"pressure", "qr", "V_state", "send"}},
+        {"P3", {"cracks", "qr", "send"}},
+        {"P4", {"qr", "disaster", "send"}},
+        {"P6", {"pressure", "qr", "disaster", "missing", "send"}}
+    };
+
+    // 連続処理のトリガー image_publisher初期化-----------------------------------------------------------------------------
+    if (pub_sub_topics.find(param) != pub_sub_topics.end()) {
+        for (const auto &topic : pub_sub_topics[param]) {
+            if(std::find(trigger_list.begin(), trigger_list.end(), topic) != trigger_list.end()){
+                image_publishers_[topic] = this->create_publisher<MyAdaptedType>(topic+"_image", 10);
+                RCLCPP_INFO(this->get_logger(), "Created publisher for topic: %s", topic.c_str());
+            }
+        }
+    } else {
+        RCLCPP_ERROR(this->get_logger(), "Invalid profile: %s", param.c_str());
+    } 
+
+    // 連続処理ノードから送られてくる結果、画像を受け取るsubscriber初期化----------------------------------------------------------
+    if (pub_sub_topics.find(param) != pub_sub_topics.end()) {
+        for (const auto &topic : pub_sub_topics[param]) {
+            if(std::find(trigger_list.begin(), trigger_list.end(), topic) != trigger_list.end()){
+                receive_data_[topic] = this->create_subscription<std_msgs::msg::String>(topic+"_result_data", 10,
+                    [this, topic](const std_msgs::msg::String::SharedPtr msg){
+                    if(topic == "qr") {
+                        latest_qr = true;
+                        id = *msg;
+                    }
+                    else {
+                        latest_topic = topic;
+                        result_data = *msg;
+                    }
+                    rewriteMessage();
+                });// 受け取り時の処理
+                receive_image_[topic] = this->create_subscription<MyAdaptedType>(topic+"_result_image",10,
+                    [this,topic](const cv::Mat msg){
+                        
+                    if(not(topic == "qr")) temporary_image = msg;
+                });// 受け取り時の処理
+                RCLCPP_INFO(this->get_logger(), "Created subscriber for topic: %s", topic.c_str());
+            }
+        }
+    } else {
+        RCLCPP_ERROR(this->get_logger(), "Invalid profile: %s", param.c_str());
+    } 
+
+    // Droneから未加工な画像(pressureなどが処理に掛ける画像)　disaster_reportやdebris_removalのため
+    receive_raw_image_ = this->create_subscription<MyAdaptedType>("raw_image",10,
+        [this](const cv::Mat msg){
+            temporary_image = msg;
+        });
+
+    // デジタルツインへ上げるpublisher初期化-----------------------------------------------------------------------------------
+    dt_qr_publisher_ = (this->create_publisher<std_msgs::msg::String>("id", 10));
+    dt_data_publisher_ = (this->create_publisher<std_msgs::msg::String>("result_data", 10));
+    dt_image_publisher_ = (this->create_publisher<MyAdaptedType>("result_image", 10));// Myadaptation
+
+    // ボタン表示設定-------------------------------------------------------------------------------------------------------
+    // ミッションごとに表示するボタンのリストを作成
+    buttons_name = pub_sub_topics[param];
+
+    // ボタンの画像を送信するpublisher初期化
+    publish_gui_ = this->create_publisher<MyAdaptedType>("drone_gui_with_buttons",1);
+
+    // ボタンのクリック判定subscriberを作成
+    click_ = this->create_subscription<geometry_msgs::msg::Point>(
+        "drone_gui_with_buttons_mouse_left", 10, std::bind(&DroneGUI::mouse_click_callback, this, std::placeholders::_1));
+
+    // ボタンの画面生成
+    mat = setup();
+
+    // 定期的にボタン画面をpublishするtimer関数
+    view_ = this->create_wall_timer(100ms, std::bind(&DroneGUI::timer_callback, this));
+
+    // 連続処理へ
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(timer_interval_ms), std::bind(&DroneGUI::publish_images, this));
+}
+
+// 画面初期化
+cv::Mat DroneGUI::setup(){
+    DrawTool canvas(width,height,0);//画面描画
+
+    for(size_t i = 0; i < buttons_name.size(); i++){
+        int row = i / btn_per_row;  // 行数
+        int col = i % btn_per_row;  // 列数
+
+        // ボタン位置を更新
+        Button btn(cv::Point(x_offset + col * (btn_width + btn_space_row), y_offset + row * (btn_height + btn_space_col)),cv::Size(btn_width,btn_height));
+        buttons_.push_back(btn); // ボタンをリストに追加
+        canvas.drawButton(btn, buttons_name[i], cv::Scalar(255, 255, 255), -1, cv::LINE_AA, 0.78, cv::Scalar(0,0,0), 2);
+    }
+
+    Button btn(cv::Point(x_offset,buttons_.back().pos.y+btn_height+15),cv::Size(btn_width*2+btn_space_row,btn_height+30));
+    buttons_.push_back(btn);
+    canvas.drawButton(buttons_.back(), "Receive: None", cv::Scalar(0, 0, 0), -1, cv::LINE_AA, 0.78, cv::Scalar(255,255,255), 2);
+
+    return canvas.getImage();
+}
+
+// 定期的にpublish
+void DroneGUI::timer_callback() {
+    if(not(mat.empty()))publish_gui_->publish(mat);
+}
+
+
+void DroneGUI::process(std::string topic_name) {
+    if(std::find(trigger_list.begin(), trigger_list.end(), topic_name) != trigger_list.end()){ //被災者の顔写真を送るのか、QRのデコードならいらないかも
+        bool_flags_[topic_name] = true;
+        start_times_[topic_name] = this->now();
+        RCLCPP_INFO_STREAM(this->get_logger(),"Push button: " << topic_name);
+    }
+    else if(not(topic_name == "send")){ //Drone PCから送られてきた画像をそのまま流す
+
+        result_data.data = "OK";// disaster, debrisは状況によって変更
+
+        if(topic_name == "V_state" && bulb_state_count == 0){
+            result_data.data = "OPEN";
+        }
+        else if(topic_name == "V_state" && bulb_state_count == 1){
+            result_data.data = "CLOSE";
+        }
+        
+        latest_topic = topic_name;
+        rewriteMessage();
+    }
+
+    if(topic_name == "send" and latest_topic != "None" and latest_qr == true and not(temporary_image.empty())){
+        dt_data_publisher_->publish(result_data);
+        dt_qr_publisher_->publish(id);
+        result_image = std::make_unique<cv::Mat>(temporary_image);
+        RCLCPP_INFO_STREAM(this->get_logger(), "address "<<&(result_image->data));
+        dt_image_publisher_->publish(std::move(result_image));
+
+        latest_topic = "None";
+        latest_qr = false;
+        temporary_image.release();
+        result_data.data = "None";
+        id.data = "None";
+        result_image = std::make_unique<cv::Mat>();
+        rewriteMessage();
+    }
+    else if(topic_name == "send" and (latest_topic == "None" or latest_qr == false or (temporary_image.empty())))RCLCPP_INFO_STREAM(this->get_logger(),"Not Prepared data");
+
+}
+
+void DroneGUI::mouse_click_callback(const geometry_msgs::msg::Point::SharedPtr msg) {
+    cv::Point point(msg->x, msg->y);
+    for(size_t i = 0; i < buttons_.size(); i++){
+        
+        // ボタンの矩形領域を定義
+        cv::Rect button_rect(buttons_[i].pos, buttons_[i].size);
+        // クリック位置がボタンの範囲内にあるかチェック
+        if (button_rect.contains(point)) {
+            cv::Point sp = cv::Point(button_rect.x, button_rect.y);
+            cv::Point ep = cv::Point(button_rect.x + btn_size.width, button_rect.y + btn_size.height);
+            std::string button_name_ = buttons_name[i];
+
+            if(button_name_ == "V_state" && bulb_state_count == 0){
+                bulb_state_count = 1;
+                rewriteButton(sp,ep,button_name_,btn_size.width,btn_size.height,cv::Scalar(0,0,255));
+            }
+            else if(button_name_ == "V_state" && bulb_state_count == 1){
+                bulb_state_count = 0;
+                rewriteButton(sp,ep,button_name_,btn_size.width,btn_size.height,cv::Scalar(255,0,0));
+            }
+            else rewriteButton(sp,ep,button_name_,btn_size.width,btn_size.height,cv::Scalar(0,0,255));
+            // クリックされたボタンを赤色にした状態でGUIを再描画
+            publish_gui_->publish(mat);
+            
+            process(button_name_);
+
+            // if(std::find(trigger_list.begin(), trigger_list.end(), button_name_) != trigger_list.end()) std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            // else std::this_thread::sleep_for(std::chrono::milliseconds(100));
+             // 1秒後に色を戻すタイマーを作成
+            color_reset_timer_ = this->create_wall_timer(
+                100ms,  // 1秒後
+                [this, sp, ep, button_name_]() {
+                    rewriteButton(sp, ep, button_name_, btn_size.width, btn_size.height, cv::Scalar(255, 255, 255));
+                    publish_gui_->publish(mat);
+                    color_reset_timer_->cancel(); // 一回で止める
+                }
+            );
+            // rewriteButton(sp,ep,button_name_,btn_size.width,btn_size.height,cv::Scalar(255,255,255));
+            publish_gui_->publish(mat);
+
+            // RCLCPP_INFO(this->get_logger(), "Button '%s' clicked",button_name_.c_str());
+        }
+    }
+}
+
+void DroneGUI::rewriteButton(cv::Point sp, cv::Point ep, std::string text, int btn_W, int btn_H, cv::Scalar color) const {
+    cv::rectangle(mat, sp, ep, color, cv::FILLED);
+    int baseline = 0;
+    cv::Size text_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX|cv::FONT_ITALIC, 0.78, 2, &baseline);
+    cv::Point tp(sp.x + (btn_W - text_size.width) / 2, sp.y + (btn_H + text_size.height) / 2);
+    cv::putText(mat, text, tp, cv::FONT_HERSHEY_SIMPLEX|cv::FONT_ITALIC, 0.78, cv::Scalar(0, 0, 0), 2);
+}
+
+void DroneGUI::rewriteMessage(){
+    std::string qr_text;
+    if(latest_qr) qr_text ="qr ";
+    else qr_text = "";
+    std::string t = "Receive: " + qr_text + latest_topic;
+    if(latest_topic == "V_state" and bulb_state_count == 1)t += "OP";
+    else if(latest_topic == "V_state" and bulb_state_count==0)t+="CL";
+
+    cv::Rect button_rect(buttons_.back().pos, buttons_.back().size);
+    cv::Point receive_btn_sp = cv::Point(button_rect.x, button_rect.y);
+    cv::Point receive_btn_ep = cv::Point(receive_btn_sp.x+buttons_.back().size.width,receive_btn_sp.y+buttons_.back().size.height);
+
+    cv::rectangle(mat, receive_btn_sp, receive_btn_ep, 0, cv::FILLED);
+    int baseline = 0;
+    cv::Size text_size = cv::getTextSize(t, cv::FONT_HERSHEY_SIMPLEX|cv::FONT_ITALIC, 0.78, 2, &baseline);
+    cv::Point tp = cv::Point(buttons_.back().pos.x + (buttons_.back().size.width - text_size.width) / 2, buttons_.back().pos.y+ (buttons_.back().size.height + text_size.height) / 2);
+    cv::putText(mat, t, tp, cv::FONT_HERSHEY_SIMPLEX|cv::FONT_ITALIC, 0.78, cv::Scalar(255, 255, 255), 2);
+}
+
+void DroneGUI::publish_images()
+{
+    for (auto &[key, flag] : bool_flags_)
+    {
+        if (flag)
+        {
+            if((this->now() - start_times_[key]).seconds() < check_duration_sec)
+            {
+                std::unique_ptr<cv::Mat> msg_image = std::make_unique<cv::Mat>(temporary_image);
+                RCLCPP_INFO_STREAM(this->get_logger(),"Publish image address: "<< &(msg_image->data));
+                image_publishers_[key]->publish(std::move(msg_image));
+            } else {
+                flag = false;
+                start_times_[key] = rclcpp::Time(0); // 任意
+            }
+        }
+    }
+}
+// void DroneGUI::publish_images()
+// {
+//     for (auto &[key, flag] : bool_flags_)
+//     {
+//         RCLCPP_INFO_STREAM(this->get_logger(), "Checking key: " << key << ", flag: " << flag);
+//         if (flag)
+//         {
+//             RCLCPP_INFO_STREAM(this->get_logger(), "Flag true for: " << key);
+//             double elapsed = (this->now() - start_times_[key]).seconds();
+//             RCLCPP_INFO_STREAM(this->get_logger(), "Elapsed time: " << elapsed << " sec");
+            
+//             if (elapsed < check_duration_sec)
+//             {
+//                 std::unique_ptr<cv::Mat> msg_image = std::make_unique<cv::Mat>(temporary_image);
+//                 RCLCPP_INFO_STREAM(this->get_logger(), "Publish image address: " << &(msg_image->data));
+//                 image_publishers_[key]->publish(std::move(msg_image));
+//             }
+//             else
+//             {
+//                 flag = false;
+//                 start_times_[key] = rclcpp::Time(0);
+//             }
+//         }
+//     }
+// }
+
+} // namespace component_operator_gui
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(component_operator_gui::DroneGUI)
